aa68ab9cc918ca29dfe003ec394b6c432946bdf9
lrbn86 <lrbn86@gmail.com>
2022-11-09 21:45:28 -0500

    Implement event/key bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'Editing Text';
-  canEdit: boolean = false;
-  message: string = 'I am read-only';
+  keyBindings: any = {
+    Action1: '1',
+    Action2: '2',
+    Action3: '3',
+    Action4: '4',
+    Action5: 'q',
+    Action6: 'w',
+    Action7: 'e',
+    Action8: 'r',
+  };
 
-  onEdit(): void {
-    this.canEdit = !this.canEdit;
+  @HostListener('window:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (event.repeat) return;
+    const key: string = event.key;
+    let actionName: string = '';
+    switch (key) {
+      case this.keyBindings.Action1:
+        actionName = 'Action1';
+        break;
+      case this.keyBindings.Action2:
+        actionName = 'Action2';
+        break;
+      case this.keyBindings.Action3:
+        actionName = 'Action3';
+        break;
+      case this.keyBindings.Action4:
+        actionName = 'Action4';
+        break;
+      case this.keyBindings.Action5:
+        actionName = 'Action5';
+        break;
+      case this.keyBindings.Action6:
+        actionName = 'Action6';
+        break;
+      case this.keyBindings.Action7:
+        actionName = 'Action7';
+        break;
+      case this.keyBindings.Action8:
+        actionName = 'Action8';
+        break;
+    }
+    if (actionName) this.handleAction(actionName);
+  }
+
+  handleActionButton(event: Event): void {
+    const target = event.currentTarget as HTMLButtonElement;
+    this.handleAction(target.name);
+  }
+
+  handleAction(actionName: string): void {
+    console.log(`Handling ${actionName}`);
   }
 }
