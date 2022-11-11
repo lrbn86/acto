ea982b2bee26d82d510442bfd4dddb516307fc7f
lrbn86 <lrbn86@gmail.com>
2022-11-10 23:07:46 -0500

    Add keyup listener and abstract checking for action name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+
   keyBindings: any = {
     Action1: '1',
     Action2: '2',
@@ -17,46 +18,53 @@ export class AppComponent {
     Action8: 'r',
   };
 
+  buttons = document.getElementsByClassName('action-button');
+
   @HostListener('window:keydown', ['$event'])
   handleKeydown(event: KeyboardEvent): void {
     if (event.repeat) return;
     const key: string = event.key;
-    let actionName: string = '';
-    switch (key) {
-      case this.keyBindings.Action1:
-        actionName = 'Action1';
-        break;
-      case this.keyBindings.Action2:
-        actionName = 'Action2';
-        break;
-      case this.keyBindings.Action3:
-        actionName = 'Action3';
-        break;
-      case this.keyBindings.Action4:
-        actionName = 'Action4';
-        break;
-      case this.keyBindings.Action5:
-        actionName = 'Action5';
-        break;
-      case this.keyBindings.Action6:
-        actionName = 'Action6';
-        break;
-      case this.keyBindings.Action7:
-        actionName = 'Action7';
-        break;
-      case this.keyBindings.Action8:
-        actionName = 'Action8';
-        break;
-    }
+    let actionName: string = this.getActionType(key);
     if (actionName) this.handleAction(actionName);
   }
 
+  @HostListener('window:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent): void {
+    const key: string = event.key;
+    let actionName: string = this.getActionType(key);
+    if (actionName) this.handleAction(actionName);
+  }
+
+  // When the user clicks on the button
   handleActionButton(event: Event): void {
     const target = event.currentTarget as HTMLButtonElement;
-    this.handleAction(target.name);
   }
 
   handleAction(actionName: string): void {
-    console.log(`Handling ${actionName}`);
+    const targetButton = this.buttons.namedItem(actionName) as HTMLButtonElement;
+    targetButton.classList.toggle('active');
   }
+
+  getActionType(key: string): string {
+    switch (key) {
+      case this.keyBindings.Action1:
+        return 'Action1';
+      case this.keyBindings.Action2:
+        return 'Action2';
+      case this.keyBindings.Action3:
+        return 'Action3';
+      case this.keyBindings.Action4:
+        return 'Action4';
+      case this.keyBindings.Action5:
+        return 'Action5';
+      case this.keyBindings.Action6:
+        return 'Action6';
+      case this.keyBindings.Action7:
+        return 'Action7';
+      case this.keyBindings.Action8:
+        return 'Action8';
+    }
+    return 'Unknown action type';
+  }
+
 }
